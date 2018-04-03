3d0fbfa8f55e23c6c74426031669de3d9f4e1e5a
Robert WC Seifert <rseifert.phone@gmail.com>
2018-04-03 08:27:49 -0400

    Added creative tab

--- src/main/java/com/builtbroken/worldofboxes/reg/WBBlocks.java
+++ src/main/java/com/builtbroken/worldofboxes/reg/WBBlocks.java
@@ -6,12 +6,16 @@ import com.builtbroken.worldofboxes.content.block.BlockCBTallGrass;
 import com.builtbroken.worldofboxes.content.block.box.BlockDimBox;
 import com.builtbroken.worldofboxes.content.block.box.TileEntityDimBox;
 import net.minecraft.block.*;
+import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemBlock;
+import net.minecraft.item.ItemStack;
 import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.registry.GameRegistry;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
 
 /**
  * @see <a href="https://github.com/BuiltBrokenModding/VoltzEngine/blob/development/license.md">License</a> for what you can and can't do with the code.
@@ -42,16 +46,27 @@ public final class WBBlocks
     @GameRegistry.ObjectHolder("tallgrass")
     public static final Block TALL_GRASS = null;
 
+    public static CreativeTabs creativeTab;
+
     @SubscribeEvent
     public static void registerBlocks(RegistryEvent.Register<Block> event)
     {
-        event.getRegistry().register(new BlockDimBox());
-        event.getRegistry().register(new BlockCBGrass());
-        event.getRegistry().register(new BlockOldLog().setUnlocalizedName(WorldOfBoxes.PREFIX + "log").setRegistryName(WorldOfBoxes.DOMAIN, "log"));
-        event.getRegistry().register(new BlockNewLog().setUnlocalizedName(WorldOfBoxes.PREFIX + "log2").setRegistryName(WorldOfBoxes.DOMAIN, "log2"));
-        event.getRegistry().register(new BlockOldLeaf().setUnlocalizedName(WorldOfBoxes.PREFIX + "leaves").setRegistryName(WorldOfBoxes.DOMAIN, "leaves"));
-        event.getRegistry().register(new BlockNewLeaf().setUnlocalizedName(WorldOfBoxes.PREFIX + "leaves2").setRegistryName(WorldOfBoxes.DOMAIN, "leaves2"));
-        event.getRegistry().register(new BlockCBTallGrass());
+        creativeTab = new CreativeTabs(WorldOfBoxes.DOMAIN)
+        {
+            @SideOnly(Side.CLIENT)
+            public ItemStack getTabIconItem()
+            {
+                return new ItemStack(BOX);
+            }
+        };
+
+        event.getRegistry().register(new BlockDimBox().setCreativeTab(creativeTab));
+        event.getRegistry().register(new BlockCBGrass().setCreativeTab(creativeTab));
+        event.getRegistry().register(new BlockOldLog().setUnlocalizedName(WorldOfBoxes.PREFIX + "log").setRegistryName(WorldOfBoxes.DOMAIN, "log").setCreativeTab(creativeTab));
+        event.getRegistry().register(new BlockNewLog().setUnlocalizedName(WorldOfBoxes.PREFIX + "log2").setRegistryName(WorldOfBoxes.DOMAIN, "log2").setCreativeTab(creativeTab));
+        event.getRegistry().register(new BlockOldLeaf().setUnlocalizedName(WorldOfBoxes.PREFIX + "leaves").setRegistryName(WorldOfBoxes.DOMAIN, "leaves").setCreativeTab(creativeTab));
+        event.getRegistry().register(new BlockNewLeaf().setUnlocalizedName(WorldOfBoxes.PREFIX + "leaves2").setRegistryName(WorldOfBoxes.DOMAIN, "leaves2").setCreativeTab(creativeTab));
+        event.getRegistry().register(new BlockCBTallGrass().setCreativeTab(creativeTab));
 
         GameRegistry.registerTileEntity(TileEntityDimBox.class, WorldOfBoxes.PREFIX + "box");
     }
@@ -66,5 +81,7 @@ public final class WBBlocks
         event.getRegistry().register(new ItemBlock(LEAF2).setRegistryName(LEAF2.getRegistryName()));
         event.getRegistry().register(new ItemBlock(GRASS).setRegistryName(GRASS.getRegistryName()));
         event.getRegistry().register(new ItemBlock(TALL_GRASS).setRegistryName(TALL_GRASS.getRegistryName()));
+
+
     }
 }
